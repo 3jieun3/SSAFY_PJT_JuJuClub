e80a432d58e5e246ad5f9300bf9d8838eec51bb6
jongil512 <jongil519@gmail.com>
2022-09-17 20:03:56 +0900

    FE settings

--- FE/joojooclub/src/router/index.js
+++ FE/joojooclub/src/router/index.js
@@ -1,84 +1,84 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from '@/store'
+// import store from '@/store'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'main',
-    component: MainView
-  },
-  {
-    path: '/recommend',
-    name: 'recommend',
-    component: RecommendView
-  },
-  {
-    path: '/recommend/result',
-    name: 'recommend',
-    component: RecommendResultView
-  },
-  {
-    path: '/drinks',
-    name: 'drinks',
-    component: DrinksView
-  },
-  {
-    path: '/drinks/:drinkPK',
-    name: 'drink',
-    component: DrinkDetailView
-  },
-  {
-    path: '/feed',
-    name: 'feed',
-    component: FeedView
-  },
-  {
-    path: '/feed/new',
-    name: 'feedNew',
-    component: PostingNewView
-  },
-  {
-    path: '/feed/:feedPK/edit',
-    name: 'feedEdit',
-    component: PostingEditView
-  },
-  {
-    path: '/profile/:userPK',
-    name: 'profile',
-    component: ProfileView
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/logout',
-    name: 'logout',
-    component: LogoutView
-  },
-  {
-    path: '/signup',
-    name: 'signup',
-    component: SignUpView
-  },
-  {
-    path: '/signout',
-    name: 'signout',
-    component: SignOutView
-  },
-  {
-    path: '/404',
-    name: 'NotFound404',
-    component: NotFound404
-  },
-  {
-    path: '*',
-    redirect: '/404'
-  },
+  // {
+  //   path: '/',
+  //   name: 'main',
+  //   component: MainView
+  // },
+  // {
+  //   path: '/recommend',
+  //   name: 'recommend',
+  //   component: RecommendView
+  // },
+  // {
+  //   path: '/recommend/result',
+  //   name: 'recommend',
+  //   component: RecommendResultView
+  // },
+  // {
+  //   path: '/drinks',
+  //   name: 'drinks',
+  //   component: DrinksView
+  // },
+  // {
+  //   path: '/drinks/:drinkPK',
+  //   name: 'drink',
+  //   component: DrinkDetailView
+  // },
+  // {
+  //   path: '/feed',
+  //   name: 'feed',
+  //   component: FeedView
+  // },
+  // {
+  //   path: '/feed/new',
+  //   name: 'feedNew',
+  //   component: PostingNewView
+  // },
+  // {
+  //   path: '/feed/:feedPK/edit',
+  //   name: 'feedEdit',
+  //   component: PostingEditView
+  // },
+  // {
+  //   path: '/profile/:userPK',
+  //   name: 'profile',
+  //   component: ProfileView
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   component: LoginView
+  // },
+  // {
+  //   path: '/logout',
+  //   name: 'logout',
+  //   component: LogoutView
+  // },
+  // {
+  //   path: '/signup',
+  //   name: 'signup',
+  //   component: SignUpView
+  // },
+  // {
+  //   path: '/signout',
+  //   name: 'signout',
+  //   component: SignOutView
+  // },
+  // {
+  //   path: '/404',
+  //   name: 'NotFound404',
+  //   component: NotFound404
+  // },
+  // {
+  //   path: '*',
+  //   redirect: '/404'
+  // },
 ]
 
 

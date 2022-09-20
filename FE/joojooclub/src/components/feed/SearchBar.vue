eea811673779d47d14efacbcc909ac4d71035a8f
박지은 <skysun704@gmail.com>
2022-09-20 17:23:16 +0900

    merge : search bar (#1)

---
+++ FE/joojooclub/src/components/feed/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="drink-search-bar">
+		<input type="text" v-model.trim="drinkName" @keyup.enter="searchDrink" placeholder="전통주명 검색">
+		<button @click="searchDrink">검색</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchBar',
+	data: function () {
+		return {
+			drinkName: '',
+		}
+	},
+	methods: {
+		searchDrink() {
+			this.drinkName = ''	// drinkName 초기화
+		}
+	}
+}
+</script>
+
+<style>
+</style>

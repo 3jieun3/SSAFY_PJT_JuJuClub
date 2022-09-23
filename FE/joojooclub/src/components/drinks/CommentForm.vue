b445a29c5a622639cf1c4a13d8e7dbb6ae29bc98
3jieun3 <skysun704@gmail.com>
2022-09-23 16:45:36 +0900

    feat : comment form rating component (#19)

--- FE/joojooclub/src/components/drinks/CommentForm.vue
+++ FE/joojooclub/src/components/drinks/CommentForm.vue
@@ -1,19 +1,36 @@
 <template>
-	<div class="comment-form">
-		<textarea name="" id="" rows="5"></textarea>
-		<button type="submit" class="btn btn-light">작성</button>
+	<div>
+		<div><rating v-model="star" type="massive star"/></div>
+		<div class="comment-form">
+			<textarea name="" id="" rows="5"></textarea>
+			<button type="submit" class="btn btn-light ">작성</button>
+		</div>
 	</div>
 </template>
 
 <script>
+import Rating from "v-rating";
+
 export default {
 	name: "CommentForm",
+	components: {
+		Rating,
+	},
+	data() {
+		return {
+			star: 0,
+		}
+	},
 }
 </script>
 
 <style>
 .comment-form {
 	display: grid;
-	grid-template-columns: 1.5fr auto;
+	gap: 1rem;
+	grid-template-columns: auto 10%;
+}
+.comment-form > button {
+	margin: 0;
 }
 </style>

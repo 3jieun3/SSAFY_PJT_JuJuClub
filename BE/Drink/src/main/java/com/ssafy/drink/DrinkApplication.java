4abab647f8426007e2b67ed0092e827d85b6083b
doseok <pkqwe37@gmail.com>
2022-09-24 12:54:07 +0900

    [BE] feat : 배포테스트

--- BE/Drink/src/main/java/com/ssafy/drink/DrinkApplication.java
+++ BE/Drink/src/main/java/com/ssafy/drink/DrinkApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DrinkApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(DrinkApplication.class, args);
+
+
+
     }
 
 }

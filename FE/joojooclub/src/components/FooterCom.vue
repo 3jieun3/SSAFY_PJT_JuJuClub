9d9482681b2e605b287eaff530e282e9aa805ad5
jongil512 <jongil519@gmail.com>
2022-09-25 19:14:26 +0900

    feat: footer 구현 (#21)

---
+++ FE/joojooclub/src/components/FooterCom.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="container d-flex align-items-start justify-content-between">
+    <div class="footerImg"></div>
+    <div class="footerCenter">
+      <div class="info1">
+        <p>Community</p>
+        <p>NOTION</p>
+        <p>GITLAB</p>
+      </div>
+      <div class="info2">
+        <p>Developers</p>
+        <p>Samsung SW Academy For Youth</p>
+        <p>정종일, 김민정, 박지은, 오도석, 최연지</p>
+      </div>
+    </div>
+    <div class="footerRight">
+      <p>Copyright @ 2022 by D106</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterCom',
+}
+</script>
+
+<style>
+
+</style>

61c5d1904b72d043c0fcaa6a5a1443941d9066c3
정종일 <jongil519@gmail.com>
2022-09-27 01:15:58 +0900

    merge: Drinks Page 기능 구현 (#12)

--- FE/joojooclub/src/components/drinks/DrinkCards.vue
+++ FE/joojooclub/src/components/drinks/DrinkCards.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="container d-flex flex-column align-items-center justify-content-center overflow-auto">
-    <div class="row">
-      <drink-card-item class="cardItems col-xl-3 col-lg-4 col-md-6 col-12"
-      v-for="(drink, index) in showPage"
-      :key="index"
-      :drink="drink"></drink-card-item>
+  <div>
+    <div class="container d-flex align-items-center justify-content-center">
+      <div class="row">
+        <drink-card-item class="cardItems col-xl-3 col-lg-4 col-md-6 col-12"
+        v-for="(drink, index) in showPage"
+        :key="index"
+        :drink="drink"></drink-card-item>
+      </div>
     </div>
-    <ul class="pagination">
-      <li class="page-item"><a @click="goPrevPage()" class="page-link">Previous</a></li>
-      <li class="page-item"
-      :class="{'active': paging.currentPage === index+1}"
-      v-for="(n, index) in totalPage"
-      :key="index"><a @click="goSpecPage(index+1)" class="page-link">{{ index+1 }}</a></li>
-      <li class="page-item"><a @click="goNextPage()" class="page-link">Next</a></li>
-    </ul>
+    <div class="pagenationWrap d-flex justify-content-center">
+      <ul class="pagination">
+        <li class="page-item"><a @click="goPrevPage()" class="page-link">Previous</a></li>
+        <li class="page-item"
+        :class="{'active': paging.currentPage === index+1}"
+        v-for="(n, index) in totalPage"
+        :key="index"><a @click="goSpecPage(index+1)" class="page-link">{{ index+1 }}</a></li>
+        <li class="page-item"><a @click="goNextPage()" class="page-link">Next</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 

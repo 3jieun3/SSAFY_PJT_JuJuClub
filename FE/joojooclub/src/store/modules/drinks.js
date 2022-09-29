eff48e6f480e7c0ba682d50b405789a1026e6625
3jieun3 <skysun704@gmail.com>
2022-09-29 16:22:49 +0900

    feat : review create and delete (#23)

--- FE/joojooclub/src/store/modules/drinks.js
+++ FE/joojooclub/src/store/modules/drinks.js
@@ -2,6 +2,7 @@ import router from "@/router"
 import axios from "axios"
 import joojooclub from "@/api/joojooclub"
 import _ from 'lodash'
+import accounts from '@/store/modules/accounts'
 // import config from '@/api_key.js'
 // import dotenv from 'dotenv';
 // dotenv.config();
@@ -234,8 +235,8 @@ export default {
       state.reviews = reviews
       state.reviewPaging.totalPage = Math.ceil(reviews.length / 5)
     },
-    CREATE_REVIEW:(state, review) => state.reviews.unshift(review),
-    DELETE_REVIEW:(state, reviewIndex) => state.reviews.splice(state.reviews.indexOf(reviewIndex), 1), 
+    //CREATE_REVIEW:(state, review) => state.reviews.unshift(review),
+    //DELETE_REVIEW:(state, reviewIndex) => state.reviews.splice(state.reviews.indexOf(reviewIndex), 1), 
     GO_PAGE(state, page) {
       // 현재 페이지를 선택된 페이지로 변경
       state.reviewPaging.currentPage = page
@@ -486,7 +487,7 @@ export default {
         method: 'get',
       }).then((res) => {
         commit('SET_DRINK', [res.data.drink, res.data.tags, res.data.foods])
-        dispatch('fetchReviews', res.data.reviews)
+        dispatch('fetchReviews', res.data.reviews.reverse())
         dispatch('goPage', 1)
       }).catch((err) => {
         console.log(err.response)
@@ -505,22 +506,20 @@ export default {
     },
     fetchReviews({ commit }, reviews) { commit('SET_REVIEWS', reviews) },
     goPage({ commit }, page) { commit('GO_PAGE', page) },
-    createReview({ commit, getters }, {drinkIndex, score, review}) {
+    createReview({ getters }, review) {
       axios({
         url: joojooclub.drinks.review(),
         method: 'post',
         headers: getters.authHeader,
-        data: { drinkIndex, score, review },
+        data: review,
       }).then(() => {
-        // review 등록 응답으로 등록된 review 정보 달라하기
-        commit('CREATE_REVIEW', drinkIndex)
-        router.push({ name: 'drink', params: { drinkPK: drinkIndex } })
+        router.push({ name: 'drinks' }) // 리다이렉트 이슈
       }).catch((err) => {
         console.log(err.response)
-        router.push({ name: 'drink', params: { drinkPK: drinkIndex }})
+        router.push({ name: 'drinks', params: { drinkPK: review.drinkIndex }})
       })
     },
-    deleteReview({ commit, getters }, reviewIndex) {
+    deleteReview({ getters }, reviewIndex) {
       if (confirm('후기를 삭제하시겠습니까?')) {
         axios({
           url: joojooclub.drinks.review(),
@@ -528,7 +527,7 @@ export default {
           headers: getters.authHeader,
           data: { reviewIndex },
         }).then(() => {
-          commit('DELETE_REVIEW', reviewIndex)
+          router.push({ name: 'drinks' })   // 리다이렉트 이슈
         }).catch((err) => {
           console.log(err.response)
         })
@@ -683,5 +682,8 @@ export default {
           commit('SET_CUSTOM_TAGS', res.data.taglist)
         })
     }
+  },
+  modules: {
+    accounts,
   },
 }

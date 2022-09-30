e278d611369e1e6ee734577c591d80d9b9b79b7c
doseok <pkqwe37@gmail.com>
2022-09-30 16:18:21 +0900

    [BE] feat : 이미지 없이 feed 업로드시 수정

--- BE/Drink/src/main/java/com/ssafy/drink/service/FeedServiceImpl.java
+++ BE/Drink/src/main/java/com/ssafy/drink/service/FeedServiceImpl.java
@@ -49,7 +49,7 @@ public class FeedServiceImpl implements FeedService{
         logger.info("피드를 작성한 멤버 정보 : {}", member);
 
         String imgUrl = null;
-        if(registFeed.getImgFile() == null) {
+        if(registFeed.getImgFile().isEmpty()) {
             imgUrl = null;
         } else {
             imgUrl = s3UploadService.upload(registFeed.getImgFile());

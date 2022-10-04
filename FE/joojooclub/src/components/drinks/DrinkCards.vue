941ef1e91a7f1437e38022812cc522d69a591032
jongil512 <jongil519@gmail.com>
2022-10-05 01:21:54 +0900

    feat: drink search 구현

--- FE/joojooclub/src/components/drinks/DrinkCards.vue
+++ FE/joojooclub/src/components/drinks/DrinkCards.vue
@@ -3,7 +3,7 @@
     <div class="container d-flex align-items-center justify-content-center">
       <div class="row cardGrid">
         <drink-card-item class="cardItems col-xl-3 col-lg-4 col-md-6 col-12"
-        v-for="(drink, index) in showPage"
+        v-for="(drink, index) in filteredDrink"
         :key="index"
         :drink="drink"></drink-card-item>
       </div>
@@ -12,19 +12,23 @@
 </template>
 
 <script>
-import { mapActions, mapGetters, mapState } from 'vuex'
+import { mapGetters } from 'vuex'
 import DrinkCardItem from './DrinkCardItem.vue'
 
 export default {
   name: 'DrinkCards',
   components: { DrinkCardItem },
   computed: {
-    ...mapState('drinks', ['drinks', 'paging']),
-    ...mapGetters('drinks', ['totalPage', 'showPage'])
+    ...mapGetters('drinks', ['showPage', 'isDrinkSearched', 'searchDrink', 'getSetFilteringDrinks']),
+    filteredDrink() {
+      if (this.isDrinkSearched) {
+        return this.getSetFilteringDrinks.filter(drink => drink.drink.drinkName === this.searchDrink[0].drink.drinkName)
+      }
+      else {
+        return this.showPage
+      }
+    }
   },
-  methods: {
-    ...mapActions('drinks', ['goPrevPage', 'goNextPage', 'goSpecPage'])
-  }
 }
 </script>
 
